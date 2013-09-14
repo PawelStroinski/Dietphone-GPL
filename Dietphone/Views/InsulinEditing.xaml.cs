1763fde0edde117f3f0d9fe96a2a224c38f4a2c8
Paweł Stroiński <pol84@live.com>
2013-09-14 22:26:16 +0100

    Add empty insulin page

---
+++ Dietphone/Views/InsulinEditing.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using Dietphone.ViewModels;
+using System.Windows.Navigation;
+using Dietphone.Tools;
+using Telerik.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Collections.Generic;
+
+namespace Dietphone.Views
+{
+    public partial class InsulinEditing : StateProviderPage
+    {
+        public InsulinEditing()
+        {
+            InitializeComponent();
+            Save = this.GetIcon(0);
+            TranslateApplicationBar();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void TranslateApplicationBar()
+        {
+            Save.Text = Translations.Save;
+            this.GetIcon(1).Text = Translations.Cancel;
+            this.GetMenuItem(0).Text = Translations.Delete;
+        }
+    }
+}
