9fb0c1a497da3ede1dfc859abd361f9697bd35ee
Paweł Stroiński <pol84@live.com>
2013-12-17 23:25:28 +0000

    Add MruProducts

---
+++ Dietphone.Common.Tests/Models/MruProductsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dietphone.Models;
+using NSubstitute;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Dietphone.Common.Tests.Models
+{
+    public class MruProductsTests
+    {
+        List<Product> products;
+        Factories factories;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            products = new Fixture().CreateMany<Product>(MruProducts.MAX_COUNT + 1).ToList();
+            factories = Substitute.For<Factories>();
+            factories.Products.Returns(products);
+        }
+
+        [Test]
+        public void Products()
+        {
+            var sut = new MruProducts(new List<Guid> { products[1].Id, products[2].Id }, factories);
+            var expected = new Product[] { products[1], products[2] };
+            var actual = sut.Products;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ProductsWhenProductDoesNotExistSkipsIt()
+        {
+            var sut = new MruProducts(new List<Guid> { Guid.NewGuid(), products[3].Id }, factories);
+            var expected = new Product[] { products[3] };
+            var actual = sut.Products;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AddProduct()
+        {
+            var sut = new MruProducts(new List<Guid> { products[0].Id }, factories);
+            sut.AddProduct(products[1]);
+            var expected = new Product[] { products[1], products[0] };
+            var actual = sut.Products;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AddProductWhenAlreadyHasMaxCountAndAddedNewProduct()
+        {
+            var initialProductIds = products.Take(MruProducts.MAX_COUNT).Select(product => product.Id).ToList();
+            var sut = new MruProducts(initialProductIds, factories);
+            sut.AddProduct(products[MruProducts.MAX_COUNT]);
+            var expected = products.Take(MruProducts.MAX_COUNT - 1).ToList();
+            expected.Insert(0, products[MruProducts.MAX_COUNT]);
+            var actual = sut.Products;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AddProductWhenAlreadyHasMaxCountAndAddedExistingProduct()
+        {
+            var initialProductIds = products.Take(MruProducts.MAX_COUNT).Select(product => product.Id).ToList();
+            var sut = new MruProducts(initialProductIds, factories);
+            sut.AddProduct(products[5]);
+            var expected = products.Take(MruProducts.MAX_COUNT).ToList();
+            expected.Remove(products[5]);
+            expected.Insert(0, products[5]);
+            var actual = sut.Products;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Dietphone.Common/Models/MruProducts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dietphone.Models
+{
+    public class MruProducts
+    {
+        public const int MAX_COUNT = 10;
+        private IList<Guid> productIds;
+        private Factories factories;
+
+        public MruProducts(IList<Guid> productIds, Factories factories)
+        {
+            this.productIds = productIds;
+            this.factories = factories;
+        }
+
+        public IList<Product> Products
+        {
+            get
+            {
+                var products = new List<Product>();
+                foreach (var id in productIds)
+                {
+                    var found = this.factories.Products.FindById(id);
+                    if (found != null)
+                        products.Add(found);
+                }
+                return products;
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (productIds.Contains(product.Id))
+                productIds.Remove(product.Id);
+            productIds.Insert(0, product.Id);
+            while (productIds.Count > MAX_COUNT)
+                productIds.RemoveAt(MAX_COUNT);
+        }
+    }
+}
