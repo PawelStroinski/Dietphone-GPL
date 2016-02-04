b2997735429d6f3e16fc754c64491bc8d202900e
Paweł Stroiński <pol84@live.com>
2016-02-04 23:09:28 +0000

    Rename the Dietphone.Phone.Tools project to Dietphone.Phone.Shared

--- Dietphone.Phone.Shared/Properties/AssemblyInfo.cs
+++ Dietphone.Phone.Shared/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dietphone Windows Phone Tools Library")]
+[assembly: AssemblyTitle("Dietphone Windows Phone Shared Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pabloware")]
