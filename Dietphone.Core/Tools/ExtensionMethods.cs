9d0c507589c80174b083d2d0cb253b7f4d358cac
Paweł Stroiński <pol84@live.com>
2016-05-01 00:40:46 +0100

    Interim commit

--- Dietphone.Core/Tools/ExtensionMethods.cs
+++ Dietphone.Core/Tools/ExtensionMethods.cs
@@ -256,7 +256,7 @@ namespace Dietphone.Tools
             if (string.IsNullOrEmpty(input))
                 return input;
             else
-                return input.Substring(0, 1).ToUpper() + input.Substring(1);
+                return input.Substring(0, 1).ToUpper() + input.Substring(1).ToLower();
         }
 
         private static bool IsAccessibleFrom(this MethodInfo method, Type type)
