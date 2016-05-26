904568010ffdffe4a5c56191468c68afef9741d1
Paweł Stroiński <pol84@live.com>
2016-05-27 00:02:45 +0100

    Interim commit

--- Dietphone.Droid/Views/EmbeddedAboutView.cs
+++ Dietphone.Droid/Views/EmbeddedAboutView.cs
@@ -3,12 +3,18 @@ using Android.OS;
 using Dietphone.Tools;
 using Dietphone.ViewModels;
 using Android.Widget;
+using System;
+using System.IO;
+using Android.Text;
+using System.Text;
 
 namespace Dietphone.Views
 {
     [Activity]
     public class EmbeddedAboutView : ActivityBase<EmbeddedAboutViewModel>
     {
+        private const string LICENSE_PATH = "documents/license.{0}.txt";
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -16,6 +22,7 @@ namespace Dietphone.Views
             Title = Translations.About.Capitalize();
             AddLinkHandler();
             AddEmailLinkHandler();
+            FormatLicense(LoadLicense());
         }
 
         private void AddLinkHandler()
@@ -28,6 +35,30 @@ namespace Dietphone.Views
         {
             var button = FindViewById<Button>(Resource.Id.about_link_email);
             button.Click += delegate { this.LaunchEmail(button.Text); };
+        }
+
+        private void FormatLicense(string text)
+        {
+            var lines = text.Split(new[] { "\r\n" }, StringSplitOptions.None);
+            var html = new StringBuilder();
+            var newParagraph = true;
+            foreach (var line in lines)
+            {
+                html.AppendLine(newParagraph
+                    ? $"<font color='#{this.ResourceColorToHex(Resource.Color.extreme_foreground)}'>{line}</font><br>"
+                    : $"{line}<br>");
+                newParagraph = line == string.Empty;
+            }
+            var target = FindViewById<TextView>(Resource.Id.license_text);
+            target.TextFormatted = Html.FromHtml(html.ToString());
+        }
+
+        private string LoadLicense()
+        {
+            var path = string.Format(LICENSE_PATH, MyApp.CurrentUiCulture);
+            using (var stream = Assets.Open(path))
+            using (var reader = new StreamReader(stream))
+                return reader.ReadToEnd();
         }
     }
 }
